3ea9329cc90d4f706c6c23aefd8c6ca96f69c93d
VJ Patel <vj@vjpatel.me>
2021-12-28 06:12:26 +0000

    Add tfstate access to gha by default

--- deployment/github-actions/gcp/main_branch.tf
+++ deployment/github-actions/gcp/main_branch.tf
@@ -10,6 +10,17 @@ resource "google_service_account" "github_repository" {
   description  = "GitHub Actions service account for ${each.key}"
 }
 
+resource "google_storage_bucket_iam_member" "github_repository" {
+  project = "vjp-terraform-state"
+
+  for_each = local.github_repository_roles
+
+  bucket = "vjp-terraform-state"
+  role   = "roles/storage.objectAdmin"
+
+  member = "serviceAccount:${google_service_account.github_repository[each.key].email}"
+}
+
 resource "google_organization_iam_member" "github_repository" {
   provider = google-beta
 
