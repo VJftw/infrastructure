1ecd74604768fe493f6350208fbf32130d1dc910
VJ Patel <vj@vjpatel.me>
2021-12-28 02:50:12 +0000

    add cloudkms

--- deployment/github-actions/gcp/repositories.tf
+++ deployment/github-actions/gcp/repositories.tf
@@ -16,22 +16,4 @@ locals {
     # }
   }
 
-  # services which GitHub Actions can use.
-  services = [
-    "billingbudgets.googleapis.com",
-    "cloudbilling.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "serviceusage.googleapis.com",
-  ]
-}
-
-resource "google_project_service" "github_actions" {
-  provider                           = google-beta
-
-  for_each = toset(local.services)
-
-  project = google_project.github_actions.project_id
-  service = each.key
-
-  disable_dependent_services = true
-}
+}  
