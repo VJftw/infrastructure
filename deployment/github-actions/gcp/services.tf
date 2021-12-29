fda068462524b3ab4635c044b2effa8d28a25eca
VJ Patel <vj@vjpatel.me>
2021-12-29 04:08:05 +0000

    add compute.googleapis.com

--- deployment/github-actions/gcp/services.tf
+++ deployment/github-actions/gcp/services.tf
@@ -8,6 +8,7 @@ locals {
     "cloudbilling.googleapis.com",
     "cloudkms.googleapis.com",
     "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
     "container.googleapis.com",
     "serviceusage.googleapis.com",
   ]
