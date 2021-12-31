576d930cccda9c06c3f46a5cf7831fa731b54512
VJ Patel <vj@vjpatel.me>
2021-12-31 17:23:35 +0000

    Add AWS Account IAM aliases

--- modules/account/aws/outputs.tf
+++ modules/account/aws/outputs.tf
@@ -5,3 +5,7 @@ output "account_id" {
 output "role_name" {
   value = aws_organizations_account.account.role_name
 }
+
+output "signin_url" {
+  value = "https://${aws_iam_account_alias.alias.account_alias}.signin.aws.amazon.com/console/"
+}
