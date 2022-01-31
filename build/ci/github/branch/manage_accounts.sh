49931a2c5bd401456e2dcd2bfb9ac8dce8d139f8
VJ Patel <VJftw@users.noreply.github.com>
2022-01-31 20:12:10 +0000

    fix branch pipeline (#27)

--- build/ci/github/branch/manage_accounts.sh
+++ build/ci/github/branch/manage_accounts.sh
@@ -6,24 +6,17 @@ source "//build/util"
 source "//build/ci/terraform:env"
 
 # Apply accounts
-mapfile -t account_tf_roots < <(./pleasew query alltargets --include terraform_workspace,accounts)
-for account_tf_root in "${account_tf_roots[@]}"; do
-    util::info "Running 'terraform apply' for ${account_tf_root}"
-
-    ./pleasew run -p "${account_tf_root}" -- "
-    terraform init -lock=true -lock-timeout=30s && \
-    terraform apply -refresh=true -compact-warnings -lock=true -lock-timeout=30s -auto-approve
-    "
-done
-
+util::info "Converging Accounts"
+./pleasew query alltargets --include terraform_workspace,accounts | ./pleasew run parallel --output=group_immediate -a "
+terraform init -lock=true -lock-timeout=30s && \
+terraform apply -refresh=true -compact-warnings -lock=true -lock-timeout=30s -auto-approve
+" -
+util::green "Converged Accounts"
 
 # Apply AWS accounts IAM
-mapfile -t iam_tf_roots < <(./pleasew query alltargets --include terraform_workspace //deployment/accounts/aws/iam/...)
-for iam_tf_root in "${iam_tf_roots[@]}"; do
-    util::info "Running 'terraform apply' for ${iam_tf_root}"
-
-    ./pleasew run -p "${iam_tf_root}" -- "
-    terraform init -lock=true -lock-timeout=30s && \
-    terraform apply -refresh=true -compact-warnings -lock=true -lock-timeout=30s -auto-approve
-    "
-done
+util::info "Converging IAM"
+./pleasew query alltargets --include terraform_workspace //deployment/accounts/aws/iam/... | ./pleasew run parallel --output=group_immediate -a "
+terraform init -lock=true -lock-timeout=30s && \
+terraform apply -refresh=true -compact-warnings -lock=true -lock-timeout=30s -auto-approve
+" -
+util::green "Converged IAM"
