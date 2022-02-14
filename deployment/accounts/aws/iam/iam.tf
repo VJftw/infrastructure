3a8515b6ec18efc049dae57a2d9e3a1b309cea50
VJ Patel <VJftw@users.noreply.github.com>
2022-02-14 23:35:29 +0000

    debug aws caller identity (#51)
    * write environment AWS credentials to default profile and unset from environment
    
    * add vjp-management auth to dns
    
    * use IAM conditions to restrict access to IAM roles

--- deployment/accounts/aws/iam/iam.tf
+++ deployment/accounts/aws/iam/iam.tf
@@ -31,11 +31,49 @@ data "aws_iam_policy_document" "assume_role_policy" {
   provider = aws.account
 
   statement {
+    sid = "AllowTrustedIAMUsers"
+
     actions = ["sts:AssumeRole", "sts:TagSession"]
     principals {
       type = "AWS"
+      # Allow the given IAM users from the given accounts.
+      identifiers = compact(flatten([
+        for account_name, iam_users in local.trusted_account_iam_users :
+        formatlist("arn:aws:iam::%s:user/%s", local.account_names_to_ids[account_name], iam_users)
+        if contains(keys(local.account_names_to_ids), account_name)
+      ]))
+    }
+
+    # Ensure that the IAM user assigns their own Username as the role session name for audit logging.
+    condition {
+      test     = "StringEquals"
+      variable = "sts:RoleSessionName"
+      values   = ["$${aws:username}"]
+    }
+  }
+
+  statement {
+    sid = "AllowOIDCProviders"
+
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+    principals {
+      type = "AWS"
+      # Broader access which is restricted by IAM condition (below): allow any principal in defined accounts.
       identifiers = formatlist("arn:aws:iam::%s:root", compact([
-        for a in data.aws_organizations_organization.org.accounts : a.id if contains(local.trusted_account_names, a.name)
+        for account_name, providers in local.trusted_account_oidc_providers : lookup(local.account_names_to_ids, account_name, "")
+      ]))
+    }
+
+    condition {
+      # Only allow principals with the given `aws:FederatedProvider` values.
+      # This is set when AssumeRoleWithWebIdentity is used (OIDC).
+      # https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_condition-keys.html#condition-keys-federatedprovider
+      test     = "StringEquals"
+      variable = "aws:FederatedProvider"
+      values = compact(flatten([
+        for account_name, providers in local.trusted_account_oidc_providers :
+        formatlist("arn:aws:iam::%s:oidc-provider/%s", local.account_names_to_ids[account_name], providers)
+        if contains(keys(local.account_names_to_ids), account_name)
       ]))
     }
   }
