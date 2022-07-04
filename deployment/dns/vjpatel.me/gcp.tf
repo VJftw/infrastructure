c37486c9e4e80d5c838632eb2fcb22b0bd555ad9
VJ Patel <VJftw@users.noreply.github.com>
2022-07-04 18:53:25 +0100

    add DNS zone for workspaces in gcp/vjp-remote-ws (#76)

--- deployment/dns/vjpatel.me/gcp.tf
+++ deployment/dns/vjpatel.me/gcp.tf
@@ -20,3 +20,49 @@ resource "google_dns_managed_zone" "gcp" {
 
   labels = {}
 }
+
+locals {
+  project_subdomains = flatten([
+    for project, subdomains in yamldecode(file("gcp_project_domains.yaml")) : [
+      for subdomain in subdomains : {
+        project   = project,
+        subdomain = subdomain,
+      }
+    ]
+  ])
+}
+
+resource "google_dns_managed_zone" "project_subdomain" {
+  for_each = {
+    for ps in local.project_subdomains : "${ps.project}:${ps.subdomain}" => ps
+  }
+
+  provider = google-beta
+
+  project = each.value.project
+
+  name = each.value.subdomain
+
+  dns_name    = "${each.value.subdomain}.${google_dns_managed_zone.gcp.dns_name}"
+  description = "DNS zone for ${each.value.subdomain}.${google_dns_managed_zone.gcp.dns_name}"
+
+  labels = {}
+}
+
+resource "google_dns_record_set" "frontend" {
+  for_each = {
+    for ps in local.project_subdomains : "${ps.project}:${ps.subdomain}" => ps
+  }
+
+  provider = google-beta
+
+  project = "vjp-dns"
+
+  name = google_dns_managed_zone.project_subdomain[each.key].dns_name
+  type = "NS"
+  ttl  = 86400
+
+  managed_zone = google_dns_managed_zone.gcp.name
+
+  rrdatas = google_dns_managed_zone.project_subdomain[each.key].name_servers
+}
