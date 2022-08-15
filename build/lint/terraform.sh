db2a53bbeae49a66df86cc2925b521f8021e301d
VJ Patel <VJftw@users.noreply.github.com>
2022-08-15 09:38:41 -0700

    Use Terraform plugin (#78)
    * Use Terraform plugin
    
    * fix refs to terraform bin
    
    * remove redundant third party defs

--- build/lint/terraform.sh
+++ build/lint/terraform.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 source "//build/util"
 
-TERRAFORM="//third_party/terraform:1_1"
+TERRAFORM="//third_party/terraform:1.1"
 
 util::infor "checking Terraform files"
 mapfile -t tf_dirs < <(./pleasew query alltargets \
